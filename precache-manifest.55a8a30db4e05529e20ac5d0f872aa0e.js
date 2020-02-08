6f9a976d9d6dd535ec1c54a7a2e77b2405525ae2
KoreaCodingKing <50541727+KoreaCodingKing@users.noreply.github.com>
2020-02-08 12:12:39 +0900

    Updates

---
+++ precache-manifest.55a8a30db4e05529e20ac5d0f872aa0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "85cd2ff0f001dd6089894c7ea4950125",
+    "url": "/Socialapp/static/media/pengsu.85cd2ff0.jpg"
+  },
+  {
+    "revision": "5a021ab95b4d2a0c8e7511c40ca7bb2f",
+    "url": "/Socialapp/static/media/no_img.5a021ab9.png"
+  },
+  {
+    "revision": "90c9579f5fa9cacd70a2",
+    "url": "/Socialapp/static/js/runtime~main.dc8e0e1d.js"
+  },
+  {
+    "revision": "bdfdd1b4c0ad67c33d73",
+    "url": "/Socialapp/static/js/main.bc9e2798.chunk.js"
+  },
+  {
+    "revision": "bf3d67eeed170c9c72fd",
+    "url": "/Socialapp/static/js/2.c5383354.chunk.js"
+  },
+  {
+    "revision": "bdfdd1b4c0ad67c33d73",
+    "url": "/Socialapp/static/css/main.b637703d.chunk.css"
+  },
+  {
+    "revision": "96466fe2604afd1786ec9b12d633cf09",
+    "url": "/Socialapp/index.html"
+  }
+];
